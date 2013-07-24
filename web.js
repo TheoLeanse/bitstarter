5a9c66d280c7f75e160f692392e29a1348f8c40e
Theodore Leanse <theoleanse@gmail.com>
2013-07-24 17:00:36 +0000

    Routed text from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
+
+var file = fs.readFileSync('index.html');
+var filetext = file.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(filetext);
 });
 
 var port = process.env.PORT || 5000;
